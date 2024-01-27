8530373eaf6243e9be5d66cae4c17abb64801392
ShubitidzeA <ana.shubitidze@quantori.com>
2024-01-27 14:22:50 +0400

    3

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 var questions = [
     {
-        text: "Which of the following is a greenhouse gas responsible for trapping heat in the Earth's atmosphere?(A) Oxygen\n (B) Nitrogen\n (C) Carbon Dioxide\n (D) Hydrogen\n",
-        answer:"C" & "c"
+        text: "Which of the following is a greenhouse gas responsible for trapping heat in the Earth's atmosphere?\n(A) Oxygen\n (B) Nitrogen\n (C) Carbon Dioxide\n (D) Hydrogen\n",
+        answer:"C" && "c"
     },
     {
         text: "Which river is the longest in the world?\n (A) Amazon River\n (B) Nile River\n (C) Mississippi River\n (D) Yangtze River \n",
-        answer:"B" & "b"
+        answer:"B" && "b"
     },
     {
         text:"Who wrote the famous play 'Romeo and Juliet'?\n (A) Charles Dickens\n (B) William Shakespeare\n (C) Jane Austen\n (D) Mark Twain\n",
-        answer: "B" & "b"
+        answer: "B" && "b"
     }
 
 ]
